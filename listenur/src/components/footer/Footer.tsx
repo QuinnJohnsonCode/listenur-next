3aad0e05aa82eb5b84dbe8f7e4d85c27d7958132
QuinnJohnsonCode <63220163+QuinnJohnsonCode@users.noreply.github.com>
2025-01-19 16:34:49 -0500

    Update footer

--- listenur/src/components/footer/Footer.tsx
+++ listenur/src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 py-4 shadow-md">
       <div className="container mx-auto text-center text-sm text-gray-400">
-        © 2024 listenur. Life's better with music. Play on! 🎧
+        © 2025 listenur. Life's better with music. Play on! 🎧
       </div>
     </footer>
   );
